ca3ca00697d86e66c1343aa1df0eb7a3df5f42ba
MeryL1997 <50187870+MeryL1997@users.noreply.github.com>
2019-08-10 15:53:31 -0500

    api conexion con Base MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express"),
     app = express(),
+    http = require("http"),
+    server = http.createServer(app),
     bodyParser  = require("body-parser"),
-    methodOverride = require("method-override");
+    methodOverride = require("method-override"),
     mongoose = require('mongoose');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,12 +12,17 @@ app.use(methodOverride());
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/hola', function(req, res) {
    res.send("Hello World!");
 });
 
 app.use(router);
 
-app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+mongoose.connect('mongodb://root:lmery1997@merylema.com:27017/ProjectGyA', function(err, res) {
+    if(err) {
+        console.log('ERROR: connecting to Database. ' + err);
+    }
+    app.listen(3000, function() {
+        console.log("Node server running on http://localhost:3000 - si funciona la conexion");
+    });
 });
